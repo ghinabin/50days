cce274eef3e3dd785c88529daee8e090e5516610
Nabin ghimire <nabinghimire@Nabins-MacBook-Pro.local>
2023-10-12 16:25:02 +0545

    variable declaration

---
+++ Day1/script.js
@@ -0,0 +1,25 @@
+/*const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+*/
+var y='hey there';
+function example(){
+    var x= 'helloworld';
+    console.log(x);
+}
+console.log(x);
+
+example();
+console.log(y);
+
